8e3bec033527e6fe37e3bbb98365e5e2446eeff3
Sara Cearc <scearc930@gmail.com>
2020-08-21 11:59:12 -0400

    added initial values for form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from "react";
+import {Route, Link, Switch} from 'react-router-dom';
+import HomePage from './HomePage';
+import PizzaForm from './PizzaForm';
+
+
+const initialFormValues = {
+  name: '',
+  pizza_size: '',
+  toppings: {
+    pepperoni: false,
+    mushroom: false,
+    pineapple: false,
+    bacon: false,
+    sausage: false,
+    onion: false,
+  },
+  special_instructions: '',
+}
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+     <div>
+     <Switch>
+     <Route path='/pizzaForm'>
+       <PizzaForm  />
+     </Route>
+
+     <Route path='/'>
+       <HomePage  />
+     </Route>
+   </Switch>
+     </div>
     </>
   );
 };
